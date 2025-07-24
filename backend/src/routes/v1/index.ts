e723a44390a923037d43fcd08c78779468e40d05
Fahim <AMFahim>
2025-07-24 11:18:02 +0600

    fix: resolved the build error

--- backend/src/routes/v1/index.ts
+++ backend/src/routes/v1/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import bookingRoute from './booking.route';
-import docsRoute from './docs.route';
 import config from '../../config/config';
 
 const router = express.Router();
@@ -24,21 +23,10 @@ const defaultRoutes = [
   }
 ];
 
-const devRoutes = [
-  {
-    path: '/docs',
-    route: docsRoute
-  }
-];
 
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
 
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
 
 export default router;
